bce8aa0a1dd6f4d8ea48df5b17d74468aa011cf0
Phelan <cody.phelan@fiserv.com>
2024-01-04 13:07:35 -0500

    cody

--- home_frontend/src/Routes/Home.jsx
+++ home_frontend/src/Routes/Home.jsx
@@ -1,44 +1,59 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ImageMagnifier from "../Components/ImageMagnifier";
-import HomeERD from "../static/Home_ERD_v2.png"
+import HomeERD from "../static/ERD_Capstone.png"
+import cavemanMeme from "../static/cavemanMemepic.png"
 import Spline from '@splinetool/react-spline';
 
 function Home() {
     const navigate = useNavigate();
 
     return (
-    <section className="bodySection snaps-inline">
+        <section className="bodySection snaps-inline">
                 {/* <div className="media-scroller snaps-inline">
                 </div> */}
-        <div className="media-element homeHero snaps-inline_child" id="homeHero">
-            <div className="text-container">
-                <h1 className="homeherrosolo">
-                Welcome to our Easilease Application, Powered by ESJ INC.
-                </h1>
-                <h4>• Seamless Transactions</h4>
-                <h4>• Secure Processing</h4>
-                <h4>• User-Friendly</h4>
 
-            </div>
-      </div>
+        <div className="cavemanMeme">
+        <img src={cavemanMeme} />
+           <div className = "cavemanText">
+                <h1 id="upperquote">"</h1>
+                <h1>So Easy I NEVER Miss Rent Payments on my Cave! </h1>
+                <h1 id="lower_quote">"</h1>
+           </div>
+        </div>
 
-      <div className="homeTeam snaps-inline_child" id="homeTeam">
+       
+       
+        <div className="media-element homeHero" id="homeHero">
+        <div className="text-container">
+            <h1 className="homeherrosolo">
+        Welcome to our Easilease Application, Powered by ESJ INC.
+        </h1>
+            <h4>• Seamless Transactions</h4>
+            <h4>• Secure Processing</h4>
+            <h4>• User-Friendly</h4>
+
+        </div>
+        </div>
+
+        <div className="homeTeam" id="homeTeam">
                     <div> </div>
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
                 </div>
-
-        <div className="homeFeatures snaps-inline_child">
-            <h1 className="features"> Our Goals </h1>
-            <div className="boxDiagram">
-                <div className="box">Improved Security</div>
-                <div className="box">Tenant / Dashboard Portal</div>
-                <div className="box">User Registration and Authentication</div>
-                <div className="box">Transaction Processing / Payment Portal</div>
-            </div>
+                
+        <div className="homeFeatures">
+            <h1 className="features">
+            Our Goals
+            </h1>
+                <div className="boxDiagram">
+                    <div className="box">Improved Security</div>
+                    <div className="box">Tenant / Dashboard Portal</div>
+                    <div className="box">User Registration and Authentication</div>
+                    <div className="box">Transaction Processing / Payment Portal</div>
+        </div>
         </div>
                
                
@@ -49,9 +64,9 @@ function Home() {
                 {/* <div className="media-element">
                 </div> */}
 
-        <div className="media-element homeTechStack snaps-inline_child" id="homeTechStack">
-            {/* <Spline scene="https://prod.spline.design/jTj8tnjowJhFSwVa/scene.splinecode" /> */}
-        </div>
+                <div className="media-element homeTechStack" id="homeTechStack">
+                    <Spline scene="https://prod.spline.design/jTj8tnjowJhFSwVa/scene.splinecode" />
+                </div>
 
         <div className="media-element homeERD snaps-inline_child" id="homeERD">
             <h1 className="erd">
@@ -59,7 +74,19 @@ function Home() {
             <ImageMagnifier imgUrl={HomeERD} />
         </div>
 
-    </section>
+                <div className="problemStatement">
+            <h1 className="problem">
+                Challenges
+            </h1>
+            <div className="problemDiagram">
+                <div className="problemBox">JWT</div>
+                <div className="problemBox">Merge Conflicts</div>
+                <div className="problemBox">Routes</div>
+                <div className="problemBox">Optimization</div>
+            </div>
+            </div>
+
+            </section>
 
     );
 }
